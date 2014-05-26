821159328faddb53461bc11413a53331e3cb9738
Chester O'Neill <c.oneill.codes@gmail.com>
2014-05-26 18:35:39 -0400

    added correct facebook callbacks

--- server/config/env/development.js
+++ server/config/env/development.js
@@ -6,8 +6,8 @@ module.exports = {
         name: 'MEAN - FullStack JS - Development'
     },
     facebook: {
-        clientID: '620640598027615',
-        clientSecret: '4b1af56e3c9282e115437b6759153546',
+        clientID: '622739847817690',
+        clientSecret: 'e3fa823ab312a9c769b1dd4da36b2258',
         callbackURL: 'http://localhost:3000/auth/facebook/callback'
     },
     twitter: {
