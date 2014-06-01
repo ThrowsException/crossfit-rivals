f56d88e406583d4e66cd38be2d7985989e3f72a0
Chester ONeill <c.oneill.codes@gmail.com>
2014-05-31 20:50:07 -0400

    facebook info added

--- server/config/env/test.js
+++ server/config/env/test.js
@@ -7,8 +7,8 @@ module.exports = {
         name: 'MEAN - A Modern Stack - Test'
     },
     facebook: {
-        clientID: 'APP_ID',
-        clientSecret: 'APP_SECRET',
+        clientID: '622739847817690',
+        clientSecret: 'e3fa823ab312a9c769b1dd4da36b2258',
         callbackURL: 'http://localhost:3000/auth/facebook/callback'
     },
     twitter: {
