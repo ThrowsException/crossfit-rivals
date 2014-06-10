7e4e73833e8ff3425f54236447083b34d252f70b
Chester ONeill <c.oneill.codes@gmail.com>
2014-06-10 17:28:47 -0400

    added facedbook config back

--- server/config/env/development.js
+++ server/config/env/development.js
@@ -6,8 +6,8 @@ module.exports = {
         name: 'MEAN - FullStack JS - Development'
     },
     facebook: {
-        clientID: 'APP_ID',
-        clientSecret: 'APP_SECRET',
+        clientID: '622739847817690',
+        clientSecret: 'e3fa823ab312a9c769b1dd4da36b2258',
         callbackURL: 'http://localhost:3000/auth/facebook/callback'
     },
     twitter: {
